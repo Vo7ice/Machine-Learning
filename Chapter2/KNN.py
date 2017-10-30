2a66aa9359b0956b1ad563d842469a03e36b3dee
Vo7ice <vo7ice@outlook.com>
2017-10-30 12:03:08 +0800

    add KNN

---
+++ Chapter2/KNN.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# 导入科学计算包numpy和运算符模块operator
+from numpy import *
+import operator
+
+
+def create_data_set():
+    """
+    创建数据集和标签
+
+     调用方式
+     import kNN
+     group, labels = kNN.createDataSet()
+    """
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify_0(inX, data_set, labels, k):
+    """
+    inx[1,2,3]
+    DS=[[1,2,3],[1,2,0]]
+    inX: 用于分类的输入向量
+    data_set: 输入的训练样本集
+    labels: 标签向量
+    k: 选择最近邻居的数目
+    注意：labels元素数目和dataSet行数相同；程序使用欧式距离公式.
+
+    预测数据所在分类可在输入下列命令
+    kNN.classify0([0,0], group, labels, 3)
+    """
+    # 1. 距离计算
+    data_set_size = data_set.shape[0]
+    # tile生成和训练样本对应的矩阵，并与训练样本求差
+    """
+    tile: 列-3表示复制的行数， 行-1／2表示对inx的重复的次数
+
+    In [8]: tile(inx, (3, 1))
+    Out[8]:
+    array([[1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]])
+
+    In [9]: tile(inx, (3, 2))
+    Out[9]:
+    array([[1, 2, 3, 1, 2, 3],
+        [1, 2, 3, 1, 2, 3],
+        [1, 2, 3, 1, 2, 3]])
+    """
+    diff_mat = tile(inX, (data_set_size, 1)) - data_set
+    """
+    欧氏距离： 点到点之间的距离
+       第一行： 同一个点 到 data_set的第一个点的距离。
+       第二行： 同一个点 到 data_set的第二个点的距离。
+       ...
+       第N行： 同一个点 到 data_set的第N个点的距离。
+
+    [[1,2,3],[1,2,3]]-[[1,2,3],[1,2,0]]
+    (A1-A2)^2+(B1-B2)^2+(c1-c2)^2
+    """
+    # 取平方
+    sq_diff_mat = diff_mat ** 2
+    # 将矩阵的每一行相加
+    sq_distances = sq_diff_mat.sum(axis=1)
+    # 开方
+    distances = sq_distances ** 0.5
+    # 根据距离排序从小到大的排序，返回对应的索引位置
+    # argsort() 是将x中的元素从小到大排列，提取其对应的index（索引），然后输出到y。
+    # 例如：y=array([3,0,2,1,4,5]) 则，x[3]=-1最小，所以y[0]=3,x[5]=9最大，所以y[5]=5。
+    # print 'distances=', distances
+    print('distances:', distances)
+    sorted_dist_indicies = distances.argsort()
+    print("souted_dist_indicies:", sorted_dist_indicies)
+
+    # 2. 选择距离最小的k个点
+    class_count = {}
+    for i in range(k):
+        # 找到该样本的类型
+        vote_label = labels[sorted_dist_indicies[i]]
+        # 在字典中将该类型加一
+        # 字典的get方法
+        # 如：list.get(k,d) 其中 get相当于一条if...else...语句,参数k在字典中，字典将返回list[k];如果参数k不在字典中则返回参数d,如果K在字典中则返回k对应的value值
+        # l = {5:2,3:4}
+        # print l.get(3,0)返回的值是4；
+        # Print l.get（1,0）返回值是0；
+        print('vote_label:', vote_label)
+        class_count[vote_label] = class_count.get(vote_label, 0) + 1
+    # 3. 排序并返回出现最多的那个类型
+    # 字典的 items() 方法，以列表返回可遍历的(键，值)元组数组。
+    # 例如：dict = {'Name': 'Zara', 'Age': 7}   print "Value : %s" %  dict.items()   Value : [('Age', 7), ('Name', 'Zara')]
+    # sorted 中的第2个参数 key=operator.itemgetter(1) 这个参数的意思是先比较第几个元素
+    # 例如：a=[('b',2),('a',1),('c',0)]  b=sorted(a,key=operator.itemgetter(1)) >>>b=[('c',0),('a',1),('b',2)] 可以看到排序是按照后边的0,1,2进行排序的，而不是a,b,c
+    # b=sorted(a,key=operator.itemgetter(0)) >>>b=[('a',1),('b',2),('c',0)] 这次比较的是前边的a,b,c而不是0,1,2
+    # b=sorted(a,key=opertator.itemgetter(1,0)) >>>b=[('c',0),('a',1),('b',2)] 这个是先比较第2个元素，然后对第一个元素进行排序，形成多级排序。
+    print('class_count:', class_count)
+    sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
+
+
+def file2matrix(filename):
+    with open(filename) as fr:
+        array_lines = fr.readlines()
+        num_lines = len(array_lines)
+        return_mat = zeros((num_lines, 3))
+        class_label_vector = []
+        index = 0
+        for line in array_lines:
+            line = line.strip()
+            list_from_line = line.split('\t')
+            return_mat[index, :] = list_from_line[0:3]
+            class_label_vector.append(int(list_from_line[-1]))
+            index += 1
+        return return_mat, class_label_vector
